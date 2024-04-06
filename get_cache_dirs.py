e9d22d0354c015a1328b4e0321b35492563726a1
Nathan Vaughn <nvaughn51@gmail.com>
2024-04-06 11:09:46 -0500

    Use simpler f string

--- get_cache_dirs.py
+++ get_cache_dirs.py
@@ -2,6 +2,8 @@ import os
 
 import pipx.paths
 
+pip_cache = os.path.join(os.path.expanduser("~"), ".cache", "pipx_pip")
+
 with open(os.environ["GITHUB_OUTPUT"], "w") as fp:
     fp.write(f"pipx={pipx.paths.ctx.venvs}")
-    fp.write(f"pip={os.path.join(os.path.expanduser("~"), ".cache", "pipx_pip")}")
+    fp.write(f"pip={pip_cache}")
