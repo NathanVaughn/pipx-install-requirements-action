bdc560c029c0c5c91a35421a58f5e61e3c0b4eb0
Nathan Vaughn <nvaughn51@gmail.com>
2024-04-06 11:21:44 -0500

    Make this simpler

--- get_cache_dirs.py
+++
@@ -1,9 +0,0 @@
-import os
-
-import pipx.paths
-
-pip_cache = os.path.join(os.path.expanduser("~"), ".cache", "pipx_pip")
-
-with open(os.environ["GITHUB_OUTPUT"], "w") as fp:
-    fp.write(f"pipx={pipx.paths.ctx.venvs}\n")
-    fp.write(f"pip={pip_cache}\n")
